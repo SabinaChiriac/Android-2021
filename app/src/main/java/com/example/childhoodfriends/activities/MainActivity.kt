9101cfecc0053188a8bed1fa02a46a391e94ca87
andreea_arvatu <arvatuandreea@yahoo.com>
2021-09-06 11:19:27 +0300

    Update activity_welcome layout and MainActivity.

---
+++ app/src/main/java/com/example/childhoodfriends/activities/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.childhoodfriends.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.childhoodfriends.R;
+
+public class MainActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
