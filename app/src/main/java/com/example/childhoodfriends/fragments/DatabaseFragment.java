ab19d4d3103c78a552883a229098ca1bb0586697
andreea_arvatu <arvatuandreea@yahoo.com>
2021-09-07 16:49:13 +0300

    Added Database fragment

---
+++ app/src/main/java/com/example/childhoodfriends/fragments/DatabaseFragment.java
@@ -0,0 +1,4 @@
+package com.example.childhoodfriends.fragments;
+
+public class DatabaseFragment {
+}
--- app/src/main/java/com/example/childhoodfriends/fragments/MyAccountFragment.java
+++ app/src/main/java/com/example/childhoodfriends/fragments/MyAccountFragment.java
@@ -1,4 +1,17 @@
 package com.example.childhoodfriends.fragments;
 
-public class MyAccountFragment {
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+
+public class MyAccountFragment extends Fragment {
+
+    private EditText editTextName;
+    private EditText editTextCity;
+    private EditText editTextNeighborhood;
+    private EditText editTextEmail;
+    private EditText editTextSchool;
+    private EditText editTextDescription;
+    private EditText editTextFreqPlaces;
+    private EditText editTextBirthday;
 }
