97da0d24b5236760ef713d9a1db43281b22b80bc
andreea_arvatu <arvatuandreea@yahoo.com>
2021-09-09 18:05:56 +0300

    Added method for getting notification on button click

--- app/src/main/java/com/example/childhoodfriends/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/childhoodfriends/activities/WelcomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.childhoodfriends.activities;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.fragment.NavHostFragment;
@@ -15,15 +18,37 @@ import com.example.childhoodfriends.R;
 import com.example.childhoodfriends.activities.LoginRegisterActivity;
 import com.example.childhoodfriends.fragments.WelcomeFragment;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
 public class WelcomeActivity extends AppCompatActivity {
 
-    private Button btnClick;
+    private Button btnGetStarted;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClickMe();
+            }
+        });
+
         onAddWelcomeFragment();
+    }
+
+    private void ClickMe() {
+        NotificationCompat.Builder mBuilder;
+        mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Started notification")
+                .setContentText("The application <Childhood friends> has started.");
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, mBuilder.build());
     }
 
     private void onAddWelcomeFragment() {
